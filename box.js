14ae86c10ba7a7d46e9dc832ad712e204219a380
chhajed23 <67373978+chhajed23@users.noreply.github.com>
2020-07-19 12:06:41 +0530

    Update box.js

--- box.js
+++ box.js
@@ -7,7 +7,7 @@
             this.body=Bodies.rectangle(x,y,width,height,options);
             this.width=width;
             this.height=height;
-            this.image=loadImage("./dustbingreen.png");
+            this.image=loadImage("dustbingreen.png");
             World.add(world,this.body);
         }
     
